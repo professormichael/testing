648619513f660ffd3f3f66928c7a2beb0d92d315
professormichael <77131080+professormichael@users.noreply.github.com>
2023-10-05 09:51:33 +0100

    Add files via upload

---
+++ artificial.js
@@ -0,0 +1,7 @@
+let now = new Date();
+let localtime = now.toString();
+let utctime = now.toGMTString();
+document.write(“<strong>Local time:</strong> “ 
++ localtime + “<br/>”);
+document.write(“<strong>UTC time:</strong> “ + utctime);
+document.body.innerHTML = "hello"
